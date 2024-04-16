cbb28403a554127abe4d8219dc2425a5500ae76f
Dorian Gonzalez <doriandarren1@gmail.com>
2024-04-16 21:00:12 +0200

    add 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/home/HomeView.vue')
+  },
+
+
+
+
+  
+
+  { 
+    path: '/:path(.*)', 
+    name: "NotFound",
+    component: () => import('@/views/sessions/404.vue'), 
+  },
+];
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/home/HomeView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
