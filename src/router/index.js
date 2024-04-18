c6515a2971d6cf7823f274a777df27eb51e690c4
Dilan <dilandarren1@gmail.com>
2024-04-18 22:15:50 +0200

    fix login

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,12 @@ const routes = [
     component: () => import('../views/home/HomeView.vue')
   },
 
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/Login.vue')
+  },
+
 
 
 
@@ -27,13 +33,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/home/HomeView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
