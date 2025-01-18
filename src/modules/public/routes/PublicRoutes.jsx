dc1b1f7e30616f084b1d0c6669e68a12498f57ec
Dorian Gonzalez <doriandarren1@gmail.com>
2025-01-18 21:13:30 +0100

    add simple template

--- src/modules/public/routes/PublicRoutes.jsx
+++ src/modules/public/routes/PublicRoutes.jsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { MainLayout } from "../../../layouts/MainLayout";
+import { AboutPage } from "../pages/AboutPage";
+import { ContactPage } from "../pages/ContactPage";
 
 export const PublicRoutes = () => {
   return (
@@ -16,6 +18,26 @@ export const PublicRoutes = () => {
             </MainLayout>
           } 
         />
+
+        <Route 
+          path="/about" 
+          element={
+            <MainLayout>
+              <AboutPage />
+            </MainLayout>
+          } 
+        />
+
+        <Route 
+          path="/contact" 
+          element={
+            <MainLayout>
+              <ContactPage />
+            </MainLayout>
+          } 
+        />
+
+
 
         {/* <Route path="/*" element={ <LoginPage /> } /> */}
         <Route path="/*" element={ <Navigate to="/login" /> } />
