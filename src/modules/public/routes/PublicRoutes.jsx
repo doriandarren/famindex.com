8e851b5d93682dadc8e85d785571e4f0f95f8922
Dorian Gonzalez <doriandarren1@gmail.com>
2025-02-23 22:37:08 +0100

    fix routes

--- src/modules/public/routes/PublicRoutes.jsx
+++ src/modules/public/routes/PublicRoutes.jsx
@@ -2,8 +2,6 @@ import { Navigate, Route, Routes } from "react-router";
 import { HomePage } from "../pages/HomePage";
 import { AboutPage } from "../pages/AboutPage";
 import { ContactPage } from "../pages/ContactPage";
-import { HeaderLayout } from "../../../layouts/public/HeaderLayout";
-import { FooterLayout } from "../../../layouts/public/FooterLayout";
 
 
 export const PublicRoutes = () => {
