73aec35d3b0834b96207ff56de0a9a94a0009242
Dorian Gonzalez <doriandarren1@gmail.com>
2025-02-28 18:46:40 +0100

    fix language

--- src/layouts/private/SessionLayout.jsx
+++ src/layouts/private/SessionLayout.jsx
@@ -70,7 +70,7 @@ export const SessionLayout = ({ children }) => {
   }));
 
 
-  const setChangeLanguaje = (event) => {
+  const setChangeLanguage = (event) => {
     const selectedLanguage = event.target.value;
     console.log(selectedLanguage);
     i18n.changeLanguage(selectedLanguage);
@@ -234,11 +234,11 @@ export const SessionLayout = ({ children }) => {
                       id="location"
                       name="location"
                       value={i18n.language}
-                      onChange={(e) => setChangeLanguaje(e)}
+                      onChange={(e) => setChangeLanguage(e)}
                       className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     >
-                      <option value="es">Español</option>
-                      <option value="en">Inglés</option>
+                      <option value="es">{ t("languages.es") }</option>
+                      <option value="en">{ t("languages.en") }</option>
                     </select>
               {/* Profile dropdown */}
               <Menu as="div" className="relative">
--- src/layouts/public/HeaderLayout.jsx
+++ src/layouts/public/HeaderLayout.jsx
@@ -43,7 +43,7 @@ export const HeaderLayout = () => {
     ],
   };
 
-  const setChangeLanguaje = (event) => {
+  const setChangeLanguage = (event) => {
     const selectedLanguage = event.target.value;
     console.log(selectedLanguage);
     i18n.changeLanguage(selectedLanguage);
@@ -370,11 +370,11 @@ export const HeaderLayout = () => {
                       id="location"
                       name="location"
                       value={i18n.language}
-                      onChange={(e) => setChangeLanguaje(e)}
+                      onChange={(e) => setChangeLanguage(e)}
                       className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     >
-                      <option value="es">Español</option>
-                      <option value="en">Inglés</option>
+                      <option value="es">{ t("languages.es") }</option>
+                      <option value="en">{ t("languages.en") }</option>
                     </select>
                     <ChevronDownIcon
                       aria-hidden="true"
