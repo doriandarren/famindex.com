f1e86eb3eb5c36898e6d4049c91bd0d0a0145b92
dilandarren <dilandarren@gmail.com>
2025-03-09 18:14:23 +0100

    add profile

--- src/modules/dashboard/routes/DashboardRoutes.jsx
+++ src/modules/dashboard/routes/DashboardRoutes.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router";
 import { DashboardPage } from "../pages/DashboardPage";
 import { TeamPage, TeamCreatePage } from "../pages";
+import { ProfilePage } from "../../auth/pages/ProfilePage";
 
 
 export const DashboardRoutes = () => {
@@ -12,6 +13,8 @@ export const DashboardRoutes = () => {
 
       <Route path="/team" element={ <TeamPage /> } />
       <Route path="/team/create" element={ <TeamCreatePage /> } />
+
+      <Route path="/profile" element={ <ProfilePage /> } />
 
       <Route path="/*" element={ <Navigate to="/auth/dashboard" /> } />
       
